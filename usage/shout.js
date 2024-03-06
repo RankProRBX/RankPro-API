a451d874eb6f31c391d29d48bd4d3780173f619f
RankProRBLX <63516612+RankProRBX@users.noreply.github.com>
2024-03-06 05:15:22 -0500

    Create shout.js

---
+++ usage/shout.js
@@ -0,0 +1,23 @@
+const { group, rankpro_api_key } = require('./config.json')
+async function shout(shoutmessage) {
+    const url = 'https://api.widgetdev.xyz/shout';
+
+    const data = {
+        "message": shoutmessage,
+        "key": rankpro_api_key,
+        "groupId": group
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    const text = await response.json();
+
+    console.log(text);
+}
+shout('Hey group!')
