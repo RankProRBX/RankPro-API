4905e2979d887c7c25d57fcb4c36e7f73c38f032
RankProRBLX <63516612+RankProRBX@users.noreply.github.com>
2024-03-06 05:13:19 -0500

    Create demote.js

---
+++ usage/demote.js
@@ -0,0 +1,22 @@
+async function demote(username) {
+    const url = 'https://api.widgetdev.xyz/demote';
+
+    const data = {
+        "target": username,
+        "key": rankpro_api_key,
+        "groupId": group
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    const text = await response.json();
+
+    console.log(text);
+}
+demote('Roblox')
