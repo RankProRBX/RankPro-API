98994317033fd048e0096300e393a6bb35fb8f15
RankProRBLX <63516612+RankProRBX@users.noreply.github.com>
2024-03-08 01:31:53 -0500

    Create searchByRoblox.js

---
+++ usage/searchByRoblox.js
@@ -0,0 +1,22 @@
+async function searchByUsername(username){
+    const url = `https://api.widgetdev.xyz/searchByUsername`;
+
+    const data = {
+        key: rankpro_api_key,
+        groupId: group,
+        username: username
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    const text = await response.json();
+
+    console.log(text);
+}
+searchByUsername('just_westtt')
